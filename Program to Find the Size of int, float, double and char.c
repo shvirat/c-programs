7dfb4c19f508f5ebaeb6d2634f4e182b20f00e42
Virat Sharma <133021376+shvirat@users.noreply.github.com>
2025-03-26 18:16:44 +0530

    Create Program to Find the Size of int, float, double and char.c

---
+++ Program to Find the Size of int, float, double and char.c
@@ -0,0 +1,19 @@
+// C Program to Find the Size of int, float, double, and char using sizeof operator.
+#include <stdio.h>
+
+int main() {
+  
+    // Print the size of int
+    printf("Size of int: %u bytes\n", sizeof(int));
+
+    // Print the size of float
+    printf("Size of float: %u bytes\n", sizeof(float));
+
+    // Print the size of double
+    printf("Size of double: %u bytes\n", sizeof(double));
+
+    // Print the size of char
+    printf("Size of char: %u bytes\n", sizeof(char));
+
+    return 0;
+}
